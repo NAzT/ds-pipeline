6080e0b74bfca7181fc5761041864feef447ff18
Nat <nat@cmmc.io>
2019-04-07 18:42:19 +0700

    response error when error.

--- api/app.py
+++ api/app.py
@@ -23,8 +23,11 @@ def predict():
         print(query_df)
         predict_val = app.model.predict(query_df).astype('float64')[0]
         return jsonify(predict=str(predict_val))
-    except:
-        return "Some input parameters are missing"
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
+        return "Some input parameters are missing " + message
 
 
 if __name__ == '__main__':
